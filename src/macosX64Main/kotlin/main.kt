4ed2a47e93e82df9a255fde1d4ba5d8bf7389a6e
Tristan <tristanferre@yahoo.fr>
2020-09-10 23:50:45 +0200

    prevent segfault

--- src/macosX64Main/kotlin/main.kt
+++ src/macosX64Main/kotlin/main.kt
@@ -7,28 +7,30 @@ import kotlinx.datetime.toInstant
 import logging.logInFile
 import services.BookingService
 
-private val preferedCities = listOf(
-    City.BOBIGNY,
-    City.GENNEVILLIERS,
-    City.MAISONS_ALFORT,
-    City.RUNGIS,
-    City.MASSY,
-    City.NOISY_LE_GRAND,
-    City.ROSNY_SOUS_BOIS,
-    City.SAINT_CLOUD,
-    City.SAINT_LEU_LA_FORET,
-    City.VELIZY_VILLACOUBLAY
-)
-
-private val minDate = LocalDateTime(2020, 9, 27, 0, 0, 0, 0).toInstant(PARIS_TIMEZONE)
-
 fun main(): Unit = runBlocking {
+    val preferedCities = listOf(
+        City.BOBIGNY,
+        City.GENNEVILLIERS,
+        City.MAISONS_ALFORT,
+        City.RUNGIS,
+        City.MASSY,
+        City.NOISY_LE_GRAND,
+        City.ROSNY_SOUS_BOIS,
+        City.SAINT_CLOUD,
+        City.SAINT_LEU_LA_FORET,
+        City.VELIZY_VILLACOUBLAY
+    )
+
+    val minDate = LocalDateTime(2020, 9, 27, 0, 0, 0, 0).toInstant(PARIS_TIMEZONE)
+
+
     val result = BookingService().tryBooking(preferedCities, minDate)
     logSuccess(result)
 }
 
 fun logSuccess(result: BookingResult) {
-    val successLog = "SUCCESS : booking taken on ${result.reservation.date} in ${result.reservation.centre} (${result.reservation.departement})"
+    val successLog =
+        "SUCCESS : booking taken on ${result.reservation.date} in ${result.reservation.centre} (${result.reservation.departement})"
     println(successLog)
     logInFile(successLog)
 }
